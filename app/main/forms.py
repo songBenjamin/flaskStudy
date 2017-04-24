9fa47d42f89493b2b21a9aed8b58ac05bf782d0a
songBenjamin <lang.song@bowhead-tech.com>
2017-04-24 23:25:54 +0800

    finish ch10

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import  Required
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import  Required, Length
 
 
 class NameFrom(FlaskForm):
     name = StringField('What is your name?',validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class EditProfileFrom(FlaskForm):
+    name = StringField('Real name', validators=[Length(0,64)])
+    location = StringField('Location', validators=[Length(0,64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
